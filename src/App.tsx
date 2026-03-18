37aeec8b1e2b4d20d6d691e33ff72afa18db16ad
MrZorawaR <nitishshyoran420@gmail.com>
2026-03-18 12:09:03 +0530

    career form fixing#2

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import AppRoutes from './routes/AppRoutes'
 import ScrollToHash from './components/utils/ScrollToHash'
+import RedirectHandler from './utils/RedirectHandler'
 
 function App() {
   return (
     <>
       <ScrollToHash />
+      <RedirectHandler />
       <AppRoutes />
     </>
   )
